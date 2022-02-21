c24b2e4700a12efa3342a3ddc1384aa098e2c52a
csernakakos <csernakakos@gmail.com>
2022-02-21 23:54:47 +0100

    Finished CRUD operations

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,63 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "GET"});
+    const goals = await Goal.find();
+    res.status(200).json({status: "success", goals});
 });
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
+    if (!req.body.text) {
         res.status(400)
         throw new Error("Please add a text field.")
     }
-    res.status(200).json({message: "SET"});
+
+    const goal = await Goal.create({text: req.body.text});
+
+    res.status(200).json({status: "success", goal});
 });
 
 // @desc    Update goal
 // @route   PUT /api/goals/:ID
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `PUT ${req.params.ID}`})
+    const goal = await Goal.findById(req.params.ID);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error("No such goal exists.")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.ID, req.body, {new: true});
+
+    res.status(200).json({status: "success", updatedGoal})
 });
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:ID
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `DELETE ${req.params.ID}`});
-});
+    const goal = await Goal.findById(req.params.ID);
 
+    if (!goal) {
+        res.status(400)
+        throw new Error("No such goal exists.");
+    }
+
+    // SOLUTION 1:
+    // await Goal.findByIdAndDelete(req.params.ID);
+
+    // SOLUTION 2:
+    await goal.remove();
+
+    res.status(200).json({status: "success", id: req.params.ID});
+});
 
 module.exports = {
     getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const colors = require("colors");
+const {connectDB} = require("./config/db");
 const dotenv = require("dotenv").config();
 const goalRoutes = require("./routes/goalRoutes");
 const {errorHandler} = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -12,4 +15,4 @@ app.use("/api/goals", goalRoutes);
 
 app.use(errorHandler);
 
-app.listen(port, () => {console.log(`mern practice on ${port}.`)})
+app.listen(port, () => {console.log(`mern practice on ${port}.`.black.bgCyan)})
